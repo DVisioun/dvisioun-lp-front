4c6c2c5600245628b47566cce6c1288d68a78fa4
Eric Kunzel <kunzel.eric@gmail.com>
2023-12-19 15:54:27 -0300

    style: changed responsiveness config for hero section + created new font size in tailwind config

--- src/components/Molecule/VideoHero/VideoHero.tsx
+++ src/components/Molecule/VideoHero/VideoHero.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 export const VideoHero = () => {
   return (
-    <div className="flex flex-col gap-4 d-0.03:w-[16rem] d-0.02:w-[20rem] d-0.1:w-[24rem]">
-        <div className="bg-gradient-to-b from-gray-500 to-dark-blue uppercase rounded-md h-[8rem] d-0.02:h-[10rem] d-0:h-[12rem] d-0.1:h-[13rem] flex justify-center items-center">
+    <div className="flex flex-col gap-4 h-auto d-0:w-[30rem] d-0.1:w-[35rem] d-0.3:w-[40rem] d-1:w-[50rem]">
+        <div className="bg-gradient-to-b min-h-[10rem] from-gray-500 to-dark-blue uppercase rounded-md flex justify-center items-center w-full h-full">
             <div>VIDEO DE VENDAS</div> {/* video here */}
         </div>
         <Button title="Quero ter meu site!"/>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,9 +26,10 @@ const config: Config = {
       base: "1rem",
       lg: "1.25rem",
       xl: "1.5rem",
-      "2xl": "2.5rem",
-      "3xl": "3rem",
-      "4xl": "4rem",
+      "2xl": "2rem",
+      "3xl": "2.5rem",
+      "4xl": "3rem",
+      "5xl": "4rem"
     },
     extend: {
       colors: {
