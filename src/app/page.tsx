bbba23979fa0db47b43c045d513458fc6bf04c03
Luana <santos.luanabeatriz1995@gmail.com>
2023-12-19 18:41:19 +0000

    Add footer (mobile)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Footer } from "@/components/Organism/Footer/Footer";
 import { Hero } from "@/components/Organism/Hero/Hero";
 
 export default function Home() {
   return (
     <main className="flex flex-col">
       <Hero />
+      <Footer />
     </main>
   )
 }
