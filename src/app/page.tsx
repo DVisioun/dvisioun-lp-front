755e7fcacf95437a052db438de273c0d697be9a6
Luana <santos.luanabeatriz1995@gmail.com>
2024-01-07 22:01:26 +0000

    Creat components reasons to

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { BoxAboutUs } from "@/components/Molecule/BoxAboutUs/BoxAboutUs";
+import ReasonsToInvest from "@/components/Molecule/ReasonsToInvest/ReasonsToInvest";
 import { Footer } from "@/components/Organism/Footer/Footer";
 import { Hero } from "@/components/Organism/Hero/Hero";
 
@@ -5,6 +7,7 @@ export default function Home() {
   return (
     <main className="flex flex-col">
       <Hero />
+      <ReasonsToInvest />
       <Footer />
     </main>
   )
